1a8ce37c9014c077f146828c4c8415d4a95baab4
Yevhenii Kravchuk <yevhenii.kravchuk@gmail.com>
2018-03-01 20:44:32 +0200

    Add rows, cols to Rules model.

--- src/main/java/tech/abcde/hashcode/model/Rules.java
+++ src/main/java/tech/abcde/hashcode/model/Rules.java
@@ -1,12 +1,17 @@
 package tech.abcde.hashcode.model;
 
 public class Rules {
-	public final long vehicleNumber;
+
+	public final long rows;
+	public final long cols;
+	public final long vehiclesAmount;
 	public final long rideBonus;
 	public final long stepsLimit;
 
-	public Rules(long vehicleNumber, long rideBonus, long stepsLimit) {
-		this.vehicleNumber = vehicleNumber;
+	public Rules(long rows, long cols, long vehiclesAmount, long rideBonus, long stepsLimit) {
+		this.rows = rows;
+		this.cols = cols;
+		this.vehiclesAmount = vehiclesAmount;
 		this.rideBonus = rideBonus;
 		this.stepsLimit = stepsLimit;
 	}
